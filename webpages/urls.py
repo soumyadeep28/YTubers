c696e8885ebad4be8dccd585100491d02bce5782
ssengup <ssengup@gmail>
2022-02-01 14:14:52 +0530

    updated for the base urls and webpages urls

---
+++ webpages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from webpages import views 
+
+urlpatterns = [ 
+    path('' , views.home ,name = "home") ,
+    path('about' , views.about ,name = "about") ,
+    path('services' , views.services ,name = "services") ,
+    path('contacts' , views.contacts ,name = "contacts") ,
+]
---
+++ webpages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    pass 
+
+def about(request):
+    pass 
+
+
+def services(request):
+    pass 
+
+
+def contacts(request):
+    pass 
