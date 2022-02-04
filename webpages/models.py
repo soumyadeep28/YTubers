f48805cbada462e91ed3db5c28ce70ce3dc2a072
ssengup <ssengup@gmail>
2022-02-04 23:08:01 +0530

    updated app/models

--- webpages/models.py
+++ webpages/models.py
@@ -1,3 +1,11 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Models(models.Models):
+    headline = models.CharField(max_length=255)         #blank=true for it i want to keep blank
+    subtitle = models.CharField(max_length=255)
+    buttontext = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='media/slider/%Y/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    
