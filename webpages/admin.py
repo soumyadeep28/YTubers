9a26cc22e8f517ab603f8f05dee81f82e161721d
ssengup <ssengup@gmail>
2022-02-05 10:35:54 +0530

    models migrated

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from webpages import models
 # Register your models here.
+admin.site.register([
+    models.Slider
+])
--- webpages/models.py
+++ webpages/models.py
@@ -2,10 +2,10 @@ from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
-class Models(models.Models):
+class Slider(models.Model):
     headline = models.CharField(max_length=255)         #blank=true for it i want to keep blank
     subtitle = models.CharField(max_length=255)
     buttontext = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='media/slider/%Y/')
     created_date = models.DateTimeField(auto_now_add=True)
-    
+
