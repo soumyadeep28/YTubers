f638242e44fb686949d0132cd6ff3b7abed9a5d1
ssengup <ssengup@gmail>
2022-02-06 23:14:05 +0530

    updated all the files upto youtubers individual page and all

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,14 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from youtubers.models import Youtubers
 
 # Create your views here.
 def youtubers(request):
-    return HttpResponse("<h1> Youtubers home page </h1>")
+    alltubers = Youtubers.objects.order_by('-created_date')
+    data ={
+        'alltubers' : alltubers , 
+    }
+
+    return render(request ,'youtubers/youtubers.html', data)
 
 
 def youtubers_detail(request ,id):
-    pass 
+    tuber = get_object_or_404(Youtubers, pk =id )
+
+    data={
+        'tuber' : tuber ,
+    }
+    return render(request ,'youtubers/youtubers_details.html', data)
 
 
 def search(request):
