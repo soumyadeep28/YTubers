4f7d6fca452ae3b36e48fdf6c30a6554ccb32aa2
ssengup <ssengup@gmail>
2022-02-07 11:14:17 +0530

    updated all the basic files for the search in the search page

--- youtubers/views.py
+++ youtubers/views.py
@@ -23,11 +23,37 @@ def youtubers_detail(request ,id):
 
 def search(request):
     tubers = Youtubers.objects.order_by('-created_date')
+
+    city_values = Youtubers.objects.values_list('city', flat=True).distinct()   #it gives key value pair and array
+    camera_type_values = Youtubers.objects.values_list('camera_type', flat=True).distinct()
+    category_values = Youtubers.objects.values_list('category', flat=True).distinct()
+    
+    #-------for filtering out the keyword values from the list
     if 'keyword' in request.GET :
         keyword = request.GET['keyword']
         if keyword :
             tubers = tubers.filter(description__icontains=keyword )
+    #-------for filtering out the city values from the array
+    if 'city' in request.GET :
+        city = request.GET['city']
+        if city :
+            tubers = tubers.filter(city__iexact = city)
+
+    if 'camera_type' in request.GET :
+        camera_type = request.GET['camera_type']
+        if camera_type :
+            tubers = tubers.filter(camera_type__iexact = camera_type)
+
+
+    if 'category' in request.GET :
+        category = request.GET['category']
+        if category :
+            tubers = tubers.filter(category__iexact = category)
+
     data ={
-        'alltubers' : tubers
+        'alltubers' : tubers,
+        'city' : city_values,
+        'camera' : camera_type_values,
+        'category' : category_values
     }
     return render(request ,'youtubers/search.html' ,data ) 
