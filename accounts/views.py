54e55191d441985cf8207305ccee015be8ba78bc
ssengup <ssengup@gmail>
2022-02-07 12:45:58 +0530

    updated views in the account application so that we can modify the basic register features

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
+
+from django.contrib.auth.models import User
+from django.contrib import messages , auth
 
 # Create your views here.
 def login(request):
     return render(request , 'accounts/login.html' )
 
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect('home')
 
 def register(request):
+    if request.method == 'POST':
+        firestname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        user =User.objects.create_user(firstname = firestname , lastname=lastname , username=username , email=email , password = password )
+        user.save()
+        messages.success(request ,'account created successfully' )
+        return redirect('login')
+
     return render(request , 'accounts/register.html' )
 
 def dashboard(request):
