0055affe7219ade7904b719eaa3dc1290f56bd5d
ssengup <ssengup@gmail>
2022-02-08 12:25:33 +0530

    updated for the basic admin pannel for contactus page in admin pannel

--- hiretubers/admin.py
+++ hiretubers/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Hiretubers)
+class HirePannel(admin.ModelAdmin):
+    list_display=('first_name' , 'tuber_name' , 'email' ,'phone' )
+    list_display_links = ( 'first_name' , 'tuber_name' , 'email')
+
+
+admin.site.register(models.Hiretubers , HirePannel)
 # Register your models here.
